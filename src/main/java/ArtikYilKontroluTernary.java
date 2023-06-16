a0d7bdb9cf3bfd7ab363489a3b45e5ed5032985d
ekimaycicek <ekimaycicek@icloud.com>
2023-06-16 14:58:21 +0300

    first commit

---
+++ src/main/java/ArtikYilKontroluTernary.java
@@ -0,0 +1,17 @@
+public class ArtikYilKontroluTernary {
+
+    public static void main(String[] args) {
+
+
+        int yil = 2024;
+
+
+        if (yil % 400 == 0 || (yil % 4 == 0 && yil % 100 != 0)) {
+
+            System.out.println(yil + " artık yıl");
+        } else {
+            System.out.println(yil + " artık yıl değil");
+        }
+
+    }
+}
